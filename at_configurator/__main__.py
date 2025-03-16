a03d7c39b33b3c36946543f87aac9c6a4b5c42a0
grigadnal625 <grigandal625@gmail.com>
2025-03-17 00:06:09 +0300

    update at config

--- at_configurator/__main__.py
+++ at_configurator/__main__.py
@@ -1,41 +1,64 @@
 import argparse
-from at_queue.core.session import ConnectionParameters
-from at_configurator.core.at_configurator import ATConfigurator
-from at_config.core.at_config_loader import CONFIG_SCHEMA, Or, Schema, Use, And, Optional
 import asyncio
-import logging
 import json
-import yaml
+import logging
 import os
 
+import yaml
+from at_config.core.at_config_loader import And
+from at_config.core.at_config_loader import CONFIG_SCHEMA
+from at_config.core.at_config_loader import Optional
+from at_config.core.at_config_loader import Or
+from at_config.core.at_config_loader import Schema
+from at_config.core.at_config_loader import Use
+from at_queue.core.session import ConnectionParameters
+
+from at_configurator.core.at_configurator import ATConfigurator
+
 parser = argparse.ArgumentParser(
-    prog='at-temporal-configurator',
-    description='General working memory for AT-TECHNOLOGY components')
+    prog="at-temporal-configurator", description="General working memory for AT-TECHNOLOGY components"
+)
 
-parser.add_argument('-u', '--url', help="RabbitMQ URL to connect", required=False, default=None)
-parser.add_argument('-H', '--host', help="RabbitMQ host to connect", required=False, default="localhost")
-parser.add_argument('-p', '--port', help="RabbitMQ port to connect", required=False, default=5672)
-parser.add_argument('-L', '--login', '-U', '--user', '--user-name', '--username', '--user_name', dest="login", help="RabbitMQ login to connect", required=False, default="guest")
-parser.add_argument('-P', '--password', help="RabbitMQ password to connect", required=False, default="guest")
-parser.add_argument('-v',  '--virtualhost', '--virtual-host', '--virtual_host', dest="virtualhost", help="RabbitMQ virtual host to connect", required=False, default="/")
+parser.add_argument("-u", "--url", help="RabbitMQ URL to connect", required=False, default=None)
+parser.add_argument("-H", "--host", help="RabbitMQ host to connect", required=False, default="localhost")
+parser.add_argument("-p", "--port", help="RabbitMQ port to connect", required=False, default=5672)
+parser.add_argument(
+    "-L",
+    "--login",
+    "-U",
+    "--user",
+    "--user-name",
+    "--username",
+    "--user_name",
+    dest="login",
+    help="RabbitMQ login to connect",
+    required=False,
+    default="guest",
+)
+parser.add_argument("-P", "--password", help="RabbitMQ password to connect", required=False, default="guest")
+parser.add_argument(
+    "-v",
+    "--virtualhost",
+    "--virtual-host",
+    "--virtual_host",
+    dest="virtualhost",
+    help="RabbitMQ virtual host to connect",
+    required=False,
+    default="/",
+)
 
-parser.add_argument('-c',  '--config', dest="config", help="Initial configuration file", required=False, default=None)
+parser.add_argument("-c", "--config", dest="config", help="Initial configuration file", required=False, default=None)
 
 
 CONFIG_DATA_SCHEMA = Or(
-    And(
-        CONFIG_SCHEMA,
-        Use(lambda value: {'config': value})
-    ),
-    Schema({
-        Optional('auth_token'): str,
-        'config': CONFIG_SCHEMA
-    }, ignore_extra_keys=True)
+    And(CONFIG_SCHEMA, Use(lambda value: {"config": value})),
+    Schema({Optional("auth_token"): str, "config": CONFIG_SCHEMA}, ignore_extra_keys=True),
 )
 
+
 async def apply_configuration(config_data: dict, configurator: ATConfigurator):
-    auth_token = config_data.get('auth_token', None)
-    config = config_data.get('config', {})
+    auth_token = config_data.get("auth_token", None)
+    config = config_data.get("config", {})
     await configurator.configurate(config=config, auth_token=auth_token)
 
 
@@ -49,28 +72,28 @@ async def main(config: str = None, **connection_kwargs):
     task = loop.create_task(configurator.start())
 
     if config is not None:
-        if config.endswith('.yml') or config.endswith('.yaml'):
+        if config.endswith(".yml") or config.endswith(".yaml"):
             data = yaml.safe_load(open(config))
-        elif config.endswith('.json'):
+        elif config.endswith(".json"):
             data = json.load(open(config))
         else:
-            raise ValueError(f'Invalid config file {config}, expected yaml, yml or json')
+            raise ValueError(f"Invalid config file {config}, expected yaml, yml or json")
         CONFIG_DATA_SCHEMA.validate(data)
         await apply_configuration(data, configurator)
     try:
-        if not os.path.exists('/var/run/at_configurator/'):
-            os.makedirs('/var/run/at_configurator/')
+        if not os.path.exists("/var/run/at_configurator/"):
+            os.makedirs("/var/run/at_configurator/")
 
-        with open('/var/run/at_configurator/pidfile.pid', 'w') as f:
+        with open("/var/run/at_configurator/pidfile.pid", "w") as f:
             f.write(str(os.getpid()))
     except PermissionError:
         pass
     await task
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     args = parser.parse_args()
     args_dict = vars(args)
-    config_file = args_dict.pop('config', None)
+    config_file = args_dict.pop("config", None)
     asyncio.run(main(**args_dict, config=config_file))
--- at_configurator/core/at_configurator.py
+++ at_configurator/core/at_configurator.py
@@ -1,35 +1,35 @@
-from uuid import UUID
-from aio_pika import IncomingMessage
-from at_config.core.at_config_handler import ATComponentConfig
-from at_queue.core.at_component import ATComponent
-from at_queue.utils.decorators import component_method, authorized_method
-from at_config.core.at_config_loader import load_config
 import asyncio
+
+from at_config.core.at_config_handler import ATComponentConfig
+from at_config.core.at_config_loader import load_config
+from at_queue.core.at_component import ATComponent
+from at_queue.utils.decorators import authorized_method
+from at_queue.utils.decorators import component_method
 
 
 class ATConfigurator(ATComponent):
-
     @component_method
     async def configurate(self, config: dict, auth_token: str = None, *args, **kwargs):
-        config = load_config(config)
-        return await asyncio.gather(*[
-            self.send_configurate(component, config, auth_token=auth_token) 
-            for component, config in config.items()
-        ])
-        
+        config = await load_config(config)
+        return await asyncio.gather(
+            *[self.send_configurate(component, config, auth_token=auth_token) for component, config in config.items()]
+        )
+
     @authorized_method
     async def authorized_configurate(self, config: dict, auth_token: str = None, *args, **kwargs):
-        config = load_config(config)
-        return await asyncio.gather(*[
-            self.send_configurate(component, config, auth_token=auth_token) 
-            for component, config in config.items()
-        ])
+        config = await load_config(config)
+        return await asyncio.gather(
+            *[self.send_configurate(component, config, auth_token=auth_token) for component, config in config.items()]
+        )
 
     async def send_configurate(self, component: str, config: ATComponentConfig, auth_token: str = None):
         if not await self.check_external_registered(component):
             raise ReferenceError(f'Component "{component}" is not registered')
-        return await self.session.send_await(component, message={
-            'type': 'configurate',
-            'config': config.__dict__,
-        }, auth_token=auth_token)
-    
+        return await self.session.send_await(
+            component,
+            message={
+                "type": "configurate",
+                "config": config.__dict__,
+            },
+            auth_token=auth_token,
+        )
